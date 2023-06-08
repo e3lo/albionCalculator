12aaae3433f738273158f96cb52d9b4a997c1197
Leo Heng <leoheng905@gmail.com>
2023-06-08 23:03:03 +1000

    Created construction node component

--- src/components/constructionNode.vue
+++ src/components/constructionNode.vue
@@ -1,9 +1,21 @@
 <template>
-    <div>
-        <h3> </h3>
+    <div id="construction-block">
+        <h3> {{ title }} </h3>
     </div>
 </template>
 
 <script setup>
+    import { defineProps } from 'vue';
 
+    defineProps({
+        title: String,
+        price: Number
+    })
 </script>
+
+<style>
+    #construction-block {
+        background-color: black;
+        color: red;
+    }
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
         <h1> Home component</h1>
+        <ConstructionNode title="Hello World" :price="200"/>
     </div>
 </template>
 
 <script setup>
+import ConstructionNode from "../components/ConstructionNode.vue";
 
 </script>
