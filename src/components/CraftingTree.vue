bfe7d2558e872053d5f5ab848fd108eb66c48ca9
Leo Heng <leoheng905@gmail.com>
2023-06-09 20:33:28 +1000

    implemented total cost calculation feature

--- src/components/CraftingTree.vue
+++ src/components/CraftingTree.vue
@@ -1,14 +1,53 @@
 <template>
-    <ConstructionNode v-for="item of recipe" :key="recipe.indexOf(item)" :title="item['title']" :price="item['price']" :quantity="item['quantity']"></ConstructionNode>
+    <div id="crafting-row">
+        <div id="crafting-row__materials">
+            <ConstructionNode v-for="item of props.recipe" :key="props.recipe.indexOf(item)" :title="item['title']" :price="item['price']" :quantity="item['quantity']"></ConstructionNode>
+        </div>
+        <div id="crafting-row__results">
+            Total Cost : {{ totalCost }}
+        </div>
+    </div>
+
 </template>
 
 <script setup>
+    import { computed } from '@vue/reactivity';
     import ConstructionNode from './ConstructionNode.vue';
     import { defineProps } from 'vue';
 
-
-    defineProps({
+    const props = defineProps({
+        title : String,
+        itemValue : Number,
+        craftingCost : Number,
         recipe : Array,
     })
 
+    // Calculate profits
+    // const albionTax = [0.08, 0.04]
+    let baseCraftingCost = 0
+    
+    for (let item of props.recipe) {
+            baseCraftingCost += item['price'] * item['quantity']
+    }
+
+    let craftingStationCost = props.itemValue * 0.1125 * (props.craftingCost / 100)
+
+    let totalCost = computed(() => {
+        return baseCraftingCost + craftingStationCost
+    })
+
 </script>
+
+<style scoped>
+
+#crafting-row {
+    display: flex;
+    flex-direction: row;
+}
+
+#crafting-row__materials {
+    display: flex;
+    flex-direction: row;
+}
+
+</style>
--- src/views/CookingPage.vue
+++ src/views/CookingPage.vue
@@ -3,14 +3,16 @@
         <h1> Cooking component</h1>
     </div>
     <div>
-        <CraftingTree :recipe="beefStew" />
+        <CraftingTree title="Beef Stew" :item-value="1060" :crafting-cost="300" :recipe="beefStew"/>
     </div>
 </template>
 
 <script setup>
     import CraftingTree from '@/components/CraftingTree.vue';
 
-    //
+    // Fetch price API [To develop]
+
+    // Recipes
     const beefStew = [
         {
             'title' : "RawBeef",
