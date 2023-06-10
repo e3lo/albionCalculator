9ff93c7c881bb4f062a15a558873cc35fbdc386e
Leo Heng <leoheng905@gmail.com>
2023-06-10 23:02:51 +1000

    Created unique buy and sell lists for future API

--- src/views/CookingPage.vue
+++ src/views/CookingPage.vue
@@ -14,6 +14,7 @@
 
 <script setup>
     import CraftingTree from '@/components/CraftingTree.vue';
+    import { computed } from '@vue/reactivity';
     import { reactive, ref } from 'vue';
 
     // Setting up Premium and Focus Toggles
@@ -21,26 +22,46 @@
     const focusToggle = ref(false)
 
     // Fetch price API [To develop]
-    /* async function refreshValues() {
-
-        let shoppingList = createShoppingList()
-
+    async function refreshValues() {
+        console.log(buyingList)
+        console.log(sellingList)
         try {
-            let response = await fetch('url')
+            //let response = await fetch('url')
             console.log("getting values")
         } catch(error) {
             console.error(`Error: ${error}`)
         }
-    } */
+    }
 
     // Creating list of items to be requested in API
-    /* function createShoppingList() {
+    const buyingList = computed(() => {
+        let list = []
 
-    } */
+        for (let items of activeRecipes.value) {
+            for (let components of items['recipe']) {
+                list.push(components['internalName'])
+            }
+        }
+
+        let uniqueList = [...new Set(list)]
+
+        return uniqueList
+    })
+
+    const sellingList = computed(() => {
+        let list = []
+
+        for (let items of activeRecipes.value) {
+            list.push(items['internalName'])
+        }
+
+        return list
+    })
 
     // Recipes
     const beefStew = reactive({
         'title' : "Beef Stew",
+        'internalName' : 'T8_MEAL_STEW',
         'itemValue' : 1060,
         'craftingCost' : 300,
         'sellPrice' : 5600,
@@ -49,17 +70,20 @@
 
         'recipe' : [
         {
-            'title' : "RawBeef",
+            'title' : "Raw beef",
+            'internalName' : 'T8_MEAT',
             'price' : 300,
             'quantity' : 72,
         },
         {
             'title' : "Pumpkin",
+            'internalName' : 'T8_PUMPKIN',
             'price' : 400,
             'quantity' : 36,
         },
         {
             'title' : "Bread",
+            'internalName' : 'T4_BREAD',
             'price' : 344,
             'quantity' : 16,
         }],    
@@ -67,6 +91,7 @@
 
     const porkOmelette = reactive({
         'title' : "Pork Omelette",
+        'internalName' : 'T7_MEAL_OMELETTE',
         'itemValue' : 1060,
         'craftingCost' : 300,
         'sellPrice' : 5400,
@@ -76,16 +101,19 @@
         'recipe' : [
         {
             'title' : "Raw Pork",
+            'internalName' : 'T7_MEAT',
             'price' : 300,
             'quantity' : 72,
         },
         {
             'title' : "Corn",
+            'internalName' : 'T7_CORN',
             'price' : 400,
             'quantity' : 36,
         },
         {
             'title' : "Goose Eggs",
+            'internalName' : 'T5_EGG',
             'price' : 344,
             'quantity' : 16,
         }],    
